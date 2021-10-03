3f027a634fd95e4a11e798d18e9ae3ff59b1c9a0
Goodness Ezeokafor <gootech442@gmail.com>
2021-10-03 18:46:40 +0100

    loaded countries from json file

--- main.go
+++ main.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/GoodnessEzeokafor/global-countries-go/countries"
+)
 
 func main() {
-	fmt.Println("GLOBAL COUNTRIES GO SDK")
+	data := countries.Countries()
+	// fmt.Println(all)
+	// birdJson := `[{"species":"pigeon","description":"likes to perch on rocks"},{"species":"eagle","description":"bird of prey"}]`
+	// var birds []Bird
+	// json.Unmarshal([]byte(birdJson), &birds)
+	fmt.Printf("Countries : %+v", data)
 }
