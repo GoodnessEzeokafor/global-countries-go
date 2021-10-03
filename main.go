0343044989df3c89d9647baeb98345c8bd0f1654
Goodness Ezeokafor <gootech442@gmail.com>
2021-10-03 19:04:44 +0100

    setting up other functionality

--- main.go
+++ main.go
@@ -1,16 +1,47 @@
 package main
 
-import (
-	"fmt"
+import "github.com/GoodnessEzeokafor/global-countries-go/countries"
 
-	"github.com/GoodnessEzeokafor/global-countries-go/countries"
-)
+/*
+ * Call this function to get all the available countries
+ * @returns all the countries
+ */
+func Countries() []countries.Country {
+	return countries.Countries()
+}
 
-func main() {
-	data := countries.Countries()
-	// fmt.Println(all)
-	// birdJson := `[{"species":"pigeon","description":"likes to perch on rocks"},{"species":"eagle","description":"bird of prey"}]`
-	// var birds []Bird
-	// json.Unmarshal([]byte(birdJson), &birds)
-	fmt.Printf("Countries : %+v", data)
+/*
+ * Call this function to get a country call code
+ * @returns all a country call code
+ */
+func GetCountryCallCode() string {
+	result := ""
+	return result
+}
+
+/*
+ * Call this function to get a country flag link
+ * @returns all a country flag
+ */
+func GetCountryFlag() string {
+	result := ""
+	return result
+}
+
+/*
+ * Call this function to get a country Alpha-2 code/ Alpha-3 code
+ * @returns Alpha-2 code/ Alpha-3 code
+ */
+func GetCountryIsoCodes() string {
+	result := ""
+	return result
+}
+
+/*
+ * Call this function to get a country capital
+ * @returns country capital
+ */
+func GetCountryCapital() string {
+	result := ""
+	return result
 }
