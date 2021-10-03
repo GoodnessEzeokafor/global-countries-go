f0e5d367941e7541279cf136f3963cedfa6f5293
Goodness Ezeokafor <gootech442@gmail.com>
2021-10-03 20:24:27 +0100

    removed main

--- countries.go
+++ countries.go
@@ -98,11 +98,3 @@ func GetCountryCapital(country string) (string, error) {
 	}
 	return result, nil
 }
-
-// func main() {
-// 	result, err := GetCountryIsoCodes("NIGERIA")
-// 	if err != nil {
-// 		fmt.Println(err)
-// 	}
-// 	fmt.Println(result)
-// }
