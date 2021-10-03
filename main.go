ea293dada28d602e7bddb45f9dd640540c507e4e
Goodness Ezeokafor <gootech442@gmail.com>
2021-10-03 19:46:03 +0100

    setup other funcitonality

--- main.go
+++ main.go
@@ -1,6 +1,11 @@
 package main
 
-import "github.com/GoodnessEzeokafor/global-countries-go/countries"
+import (
+	"errors"
+	"strings"
+
+	"github.com/GoodnessEzeokafor/global-countries-go/countries"
+)
 
 /*
  * Call this function to get all the available countries
@@ -14,34 +19,82 @@ func Countries() []countries.Country {
  * Call this function to get a country call code
  * @returns all a country call code
  */
-func GetCountryCallCode() string {
-	result := ""
-	return result
+func GetCountryCallCode(country string) (string, error) {
+	if country == "" {
+		return "", errors.New("invalid country")
+	}
+	var result string
+	countries := countries.Countries()
+	for i := 0; i < len(countries); i++ {
+		if countries[i].Country == strings.ToLower(country) {
+			result = countries[i].Code
+		}
+	}
+	if result == "" {
+		return "", errors.New("invalid country")
+	}
+	return result, nil
 }
 
 /*
  * Call this function to get a country flag link
  * @returns all a country flag
  */
-func GetCountryFlag() string {
-	result := ""
-	return result
+func GetCountryFlag(country string) (string, error) {
+	if country == "" {
+		return "", errors.New("invalid country")
+	}
+	var result string
+	countries := countries.Countries()
+	for i := 0; i < len(countries); i++ {
+		if countries[i].Country == strings.ToLower(country) {
+			result = countries[i].Flag
+		}
+	}
+	if result == "" {
+		return "", errors.New("invalid country")
+	}
+	return result, nil
 }
 
 /*
  * Call this function to get a country Alpha-2 code/ Alpha-3 code
  * @returns Alpha-2 code/ Alpha-3 code
  */
-func GetCountryIsoCodes() string {
-	result := ""
-	return result
+func GetCountryIsoCodes(country string) (string, error) {
+	if country == "" {
+		return "", errors.New("invalid country")
+	}
+	var result string
+	countries := countries.Countries()
+	for i := 0; i < len(countries); i++ {
+		if countries[i].Country == strings.ToLower(country) {
+			result = countries[i].Isocodes
+		}
+	}
+	if result == "" {
+		return "", errors.New("invalid country")
+	}
+	return result, nil
 }
 
 /*
  * Call this function to get a country capital
  * @returns country capital
  */
-func GetCountryCapital() string {
-	result := ""
-	return result
+func GetCountryCapital(country string) (string, error) {
+	if country == "" {
+		return "", errors.New("invalid country")
+	}
+	var result string
+	countries := countries.Countries()
+	for i := 0; i < len(countries); i++ {
+		if countries[i].Country == strings.ToLower(country) {
+			result = countries[i].Capital
+		}
+	}
+	if result == "" {
+		return "", errors.New("invalid country")
+	}
+	return result, nil
 }
